73405c2643774eb1d18a4ff3b134bbfc1ede3d09
alex025x <>
2023-09-07 13:41:11 +0000

    Set function to populate tiles within the board

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,27 @@ var columns = 7;
 window.onload = function () {
     setGame();
 };
+
+
+function setGame(rows, columns) {
+    // Create an empty game board as a 2D array
+    let board: [];
+
+
+    // Loop to create rows
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+
+        // Loop to create columns
+        for (let c = 0; c < columns; c++) {
+
+            // Initialize each cell with an empty value
+            row.push('');
+
+            // Create an HTML element for the cell
+            let title = document.createElement("div");
+            title.id = r.toString() + "-" + c.toString();
+
+        }
+    }
+}
