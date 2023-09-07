b4dc898c7f8edd9e4850c1e2558af3dfc51f0590
alex025x <>
2023-09-07 15:49:03 +0000

    Add function to check for win vertically

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,14 +78,22 @@ function setPiece() {
 }
 
 function checkWinner() {
-    // check winner horizontally
+    // Check for a horizontal win
     for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns -3; c++) {
-            if (board[r][c] != '') {
-                if (board[r][c] == board[r][c + 1] && board[r][c + 1] == board[r][c + 2] && board[r][c + 2] == board[r][c + 3]) {
-                    setWinner(r, c);
-                    return;
-                }
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] !== '' && board[r][c] === board[r][c + 1] && board[r][c + 1] === board[r][c + 2] && board[r][c + 2] === board[r][c + 3]) {
+                setWinner(r, c);
+                return;
+            }
+        }
+    }
+
+    // Check for a vertical win
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] !== '' && board[r][c] === board[r + 1][c] && board[r + 1][c] === board[r + 2][c] && board[r + 2][c] === board[r + 3][c]) {
+                setWinner(r, c);
+                return;
             }
         }
     }
